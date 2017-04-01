e9862a79ebaa9faa2df0a3abc70c61032437c7ac
Cantidio Fontes <aniquilatorbloody@gmail.com>
2017-03-31 23:52:47 -0400

    adds read pin

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,9 +1,14 @@
 var gpio = require('rpi-gpio');
 var togglePin = require('../lib/toggle-pin');
 
-const pin = 16;
-gpio.setup(pin, gpio.DIR_OUT, function() {
-  togglePin(pin, 1000);
+gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
 });
+
+gpio.setup(16, gpio.DIR_OUT, function() {
+  togglePin(16, 1000);
+});
+
+gpio.setup(40, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 process.stdin.resume();
